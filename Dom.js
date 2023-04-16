86722448e7712a2db8819d83222f91ec8aec7710
mustafamj9 <mdmustafajaveed007@gmail.com>
2023-04-16 23:36:30 +0530

    DOM & BOM intro

---
+++ Dom.js
@@ -0,0 +1,12 @@
+console.log(window);
+// window.console.log("a");
+// window is global object.
+
+// window.console.log("a");
+
+// DOM
+console.log(document);
+// document.body.style.backgroundColor = "blue"
+
+// BOM
+location.href = "http://localhost"
---
+++ prompt.js
@@ -0,0 +1,4 @@
+// prompt takes an input from the user and
+
+// let a = prompt("enter the number");
+// document.write(a);
