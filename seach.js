ecfaaba2814d244007e8239fcc11b3ebdd309f76
mustafamj9 <mdmustafajaveed007@gmail>
2023-04-23 07:28:35 +0530

    search DOM

---
+++ seach.js
@@ -0,0 +1,12 @@
+// work if a sholud be element
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let a = document.getElementById("ctitle");
+//     a.style.color = "yellow";
+//   });
+  
+document.addEventListener("DOMContentLoaded", function() {
+    let a = document.getElementsByName("card-title")[0];
+    a.style.color = "yellow";
+  });
+  
